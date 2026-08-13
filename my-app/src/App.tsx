bd7b469b70e4d52af1f2e761a9bd3125d8d70195
guilherme.campos <guilherme.76campos@gmail.com>
2026-08-13 20:17:07 -0300

    feat: Iniciando com git flow + Componetização em Vite + React

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Guilherme from "./components/Nome/Guilherme";
+
+export default function App() {
+
+  return (
+  <div>
+    <h1>Olá Mundo!</h1>
+    <Guilherme/>
+  </div>
+)
+  
+}
---
+++ my-app/src/components/Nome/Guilherme.tsx
@@ -0,0 +1,8 @@
+export default function Guilherme() {
+    return(
+        <div>
+            <h2>Guilherme</h2>
+        </div>
+    )
+    
+}
