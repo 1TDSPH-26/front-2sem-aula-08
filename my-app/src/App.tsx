6b7fd1b705ad7f410d20f0f8f217887e4b7a9208
felipemotitsukitan <felipetan13@gmail.com>
2026-08-19 09:46:59 -0300

    feat: Trabalhando com props, types, cards.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,7 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho"
+import Conteudo from "./components/Conteudo/Conteudo"
+import Rodape from "./components/Rodape/Rodape"
+
 
 export default function App(){
 
@@ -6,21 +9,8 @@ export default function App(){
         <div>
 
             <Cabecalho/>
-            <Cabecalho />
-            <Cabecalho />
-            <Cabecalho />
-            <Cabecalho />
-            <Cabecalho />
-
-            <main>
-                <h2>
-                    Conteúdo aleatório
-                </h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reprehenderit iure illum dolorum id fuga eum suscipit repellendus? Porro ab voluptatum totam sed, suscipit perspiciatis ea pariatur quos quisquam fuga.</p>
-            </main>
-            <footer>
-                <p>Todos os direitor resevados &copy;</p>
-            </footer>
+            <Conteudo/>
+            <Rodape/>
         </div>
     );
 }
