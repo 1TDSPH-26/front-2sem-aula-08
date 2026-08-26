ba15bd695f5e514c85286b038d74ac9ada759430
felipemotitsukitan <felipetan13@gmail.com>
2026-08-26 09:48:32 -0300

    feat: Rotas com react-router, implantando menu modular e instalando tailwindcss.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router";
 import Cabecalho from "./components/Cabecalho/Cabecalho"
 import Conteudo from "./components/Conteudo/Conteudo"
 import Rodape from "./components/Rodape/Rodape"
@@ -7,9 +8,8 @@ export default function App(){
 
     return(
         <div>
-
             <Cabecalho/>
-            <Conteudo/>
+                <Outlet/>
             <Rodape/>
         </div>
     );
