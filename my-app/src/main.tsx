fb101eca52726231b770ff247cf8d45279a0c49f
marifernandesgm <marifernandesgm17@gmail.com>
2026-08-26 09:52:48 -0300

    feat: Rotas com react-router, implantando menu modular e instalando tailwindcss.

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.tsx'
+
+//Importando os objetos do react-router
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+//Importando os componentes das rotas
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
+import EditarProdutos from './routes/EditarProdutos/index.tsx'
+import Erro from './routes/Error/index.tsx'
+
+//Criando o vetor de objetos com rotas
+const router = createBrowserRouter([
+  {path: "/", element: <App />, errorElement: <Erro />, children: [
+    {path: "/", element: <Home />},
+    {path: "/produtos", element: <Produtos />},
+    {path: "/editar-produtos/:id", element: <EditarProdutos />},
+  ]}
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
