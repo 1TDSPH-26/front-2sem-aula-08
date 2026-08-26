6636077668979b651d222181c69be41ce16bd051
Rikk <tarikmoussa90@gmail.com>
2026-08-26 09:48:54 -0300

    feat: rotas com react-router, implantando menus

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.tsx'
 
+//Importando os objetos do react rounter
+import {createBrowserRouter, RouterProvider } from 'react-router';
+
+//importando os componentes de rotas
+import Home from './routes/Home/index.tsx';
+import Produtos from './routes/Produtos/index.tsx';
+import EditarProdutos from './routes/EditarProdutos/index.tsx';
+import Error from './routes/Error/index.tsx';
+
+
+//Criando o vetor de objetos com rotas
+const router = createBrowserRouter ([
+{path: "/", element: <App/>, errorElement: <Error/>, children: [
+{path: "/", element: <Home/>},
+{path: "/produtos", element: <Produtos/>},
+{path: "/editar-produtos/:id", element: <EditarProdutos/>}, 
+]}]);
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<StrictMode>
+  <RouterProvider router={router} />
+</StrictMode>,
 )
