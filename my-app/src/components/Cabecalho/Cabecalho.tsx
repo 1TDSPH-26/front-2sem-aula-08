a298378651f0160f65f059498985d3d97ca158dc
Luigi Tormim <luiginhotoca@gmail.com>
2026-08-26 09:46:47 -0300

    feat :rotas com react-router, implantando menu modular

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,13 @@
+import Menu from "../Menu/Menu";
+
 export default function Cabecalho(){
 
     return (
         <header>
             <h1>Meu Componente</h1>
-            <button>Teste</button>
+            <Menu/>
         </header>
     );
 }
+
+
