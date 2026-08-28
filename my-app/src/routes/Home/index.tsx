cc31261ea8cd3ae12c99e9bed055c1668af40c61
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-08-27 22:28:06 -0300

    feat: modificando a home, fazendo com que ela tenha um botão que leve diretamente a aba de produtos, estilizando ela com o tailwind css, e adicionando uma imagem ao hero da página.

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -1,7 +1,26 @@
-export default function Home(){
-    return(
-        <main className="min-h-screen bg-green-400 text-3xl">
-            <h1>home</h1>
+import { useNavigate } from 'react-router'
+import ImagemHero from '../../assets/ImagemHero.png'
+
+export default function Home() {
+    const navigate = useNavigate()
+    const botaoHero = () =>{
+        navigate("/produtos")
+    }
+
+    return (
+        <main className="font-bold min-h-screen">
+            <div className="w-5xl h-60">
+                <img className="absolute h-120 w-full object-cover" src={ImagemHero} alt="fundo branco com as bordas em azul claro" />
+                <div className="inset-0 absolute flex flex-col items-center mt-50 text-3xl">
+                    <h2>Acesse os melhores pordutos por preços acessíveis</h2>
+                    <button
+                    onClick={() => botaoHero()}
+                     className="mt-5 border-2 border-sky-300 w-60 rounded-full hover:bg-sky-300 hover:text-white transition-colors duration-300 p-1">
+                        Clique Aqui
+                        </button>
+                </div>
+            </div>
+
         </main>
     )
 }
