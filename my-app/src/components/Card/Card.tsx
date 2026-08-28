6e9390bbb6febe104c75f9840904b862deafd3e0
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-08-27 22:24:48 -0300

    feat: Estilizando o card, fazendo com que ele comporte imagens dos produtos atraves da url, e estilizando ele com tailwind css

--- my-app/src/components/Card/Card.tsx
+++ my-app/src/components/Card/Card.tsx
@@ -1,18 +1,25 @@
-type TipoProps ={
+type TipoProps = {
     nomeProduto: string
     preco: string
+    url: string
 }
 
 
-export default function Card( {nomeProduto, preco}:TipoProps) {
-   
-   
-    return(
-        <div>
-            <h2>Card</h2>
-            <p>Nome Produto: {nomeProduto}</p>
-            <p>Preco: {preco}</p>
+export default function Card({ nomeProduto, preco, url }: TipoProps) {
+
+
+    return (
+        <div className="w-65 text-justify h-65 border-3 rounded-3xl border-sky-200 shadow-xl hover:w-72 hover:h-72 transition-all hover:shadow-2xl duration-200">
+            <div className="p-4 mt-2">
+                <p>Nome Produto: {nomeProduto}</p>
+                <div className="flex justify-center">
+                    <img className="w-35 py-2 " src={url} alt="Imagem varia de acordo com o nome do produto" />
+                </div>
+
+                <p>Preco: {preco}</p>
+            </div>
+
         </div>
-        
+
     )
 }
