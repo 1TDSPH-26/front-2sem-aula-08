7991222eb2abb189960a3196ffb3f582ce05dff0
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-08-27 22:29:04 -0300

    feat: Adicionando o botaoHome para que p usuário consiga ter uma caminho mais fácil ate a página home do site, e estilizando ela com o tailwind css.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,8 +1,28 @@
-export default function Produtos(){
+import { useNavigate } from "react-router";
+import Conteudo from "../../components/Conteudo/Conteudo";
 
-    return(
-        <main>
-            <h2>Produtos</h2>
+export default function Produtos() {
+    const navigate = useNavigate()
+    const botaoHome = () => {
+        navigate("/")
+    }
+
+
+    return (
+        <main className="min-h-screen font-bold">
+            <div className="p-3">
+                <button
+                    onClick={() => botaoHome()}
+                    className="mt-5 border-2 border-sky-300 w-52 rounded-full hover:bg-sky-300 hover:text-white transition-colors duration-300 p-1">
+                    Voltar para home
+                </button>
+            </div>
+            <div className="flex justify-center pt-4">
+                <h1 className=" text-4xl">Produtos</h1>
+            </div>
+            <div>
+                <Conteudo />
+            </div>
         </main>
     )
 
