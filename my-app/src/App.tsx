886d2160e0086df2732174eb4a25e57faf716602
giovannidevazevedo <giovanniazevedo065@gmail.com>
2026-08-28 08:36:01 -0300

    feat: Adiciona rotas e configurações do Tailwind CSS

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router"
 import Cabecalho from "./components/Cabecalho/Cabecalho"
 
 export default function App(){
@@ -6,21 +7,8 @@ export default function App(){
         <div>
 
             <Cabecalho/>
-            <Cabecalho />
-            <Cabecalho />
-            <Cabecalho />
-            <Cabecalho />
-            <Cabecalho />
-
-            <main>
-                <h2>
-                    Conteúdo aleatório
-                </h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste reprehenderit iure illum dolorum id fuga eum suscipit repellendus? Porro ab voluptatum totam sed, suscipit perspiciatis ea pariatur quos quisquam fuga.</p>
-            </main>
-            <footer>
-                <p>Todos os direitor resevados &copy;</p>
-            </footer>
+             <Outlet/>
+            
         </div>
     );
 }
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './routes/Home/index.tsx'
+import Produtos from './routes/Produtos/index.tsx'
+import Error from './routes/Error/index.tsx'
 import App from './App.tsx'
+
+
+const router = createBrowserRouter([{
+  path: "/", element: <App />, errorElement: <Error />, children: [
+    {path: "/", element: <Home />},
+    {path: "/produtos", element: <Produtos />}
+  ]
+}])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
