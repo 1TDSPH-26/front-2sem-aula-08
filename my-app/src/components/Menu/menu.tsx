fd376f898f523f10d67d51f1374f6afa367eb39c
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-08-27 22:26:23 -0300

    feat: Estilizando o menu com tailwind css

--- my-app/src/components/Menu/menu.tsx
+++ my-app/src/components/Menu/menu.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router";
 
 export default function Menu(){
+    const estiloNav = "text-white hover:text-sky-600 transition-color duration-300"
+
     return(
           <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/produtos">Produtos</Link></li>
+                <ul className="flex gap-2 font-bold ">
+                    <li><Link className={estiloNav} to="/">Home</Link></li>
+                    <li><Link className={estiloNav} to="/produtos">Produtos</Link></li>
                 </ul>
             </nav>
     )
