0a4800abaed91827cb4381241040be0ab590b834
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-08-27 22:26:50 -0300

    Adicionando o link da organização 1TDSPH no rodape do site.

--- my-app/src/components/Rodape/Rodape.tsx
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -6,8 +6,8 @@ export default function Rodape(){
 
     return(
         <div>
-            <footer>
-                <p>Todos os direitos reservados &copy;</p>
+            <footer className="mt-auto">
+                <p>Todos os direitos reservados &copy;  ------- Contato da Equipe: <a href="https://github.com/1TDSPH-26">1TDSPH</a></p>
             </footer>
         </div>
     )
