e01e854c4f559b4c067a19218571e087aabab832
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-08-27 22:23:15 -0300

    feat: Estilizando o cabeçalho da página com tailwindcss

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -3,8 +3,8 @@ import Menu from "../Menu/menu";
 export default function Cabecalho(){
 
     return (
-        <header>
-            <h1>Meu Componente</h1>
+        <header className="text-white bg-blue-300 font-bold w-ful h-10 flex text-lg justify-between py-2 px-15">
+            <h1>Boiler Plate</h1>
             <Menu/>
         </header>
     );
